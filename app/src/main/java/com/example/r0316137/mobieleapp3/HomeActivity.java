03f68ae0e740f6fe8c549df58e54033e9799faff
Boelen <tom.boelen@student.khlim.be>
2015-12-13 22:29:01 +0100

    HomeActivityButtons - functionaliteit toegevoegd

--- app/src/main/java/com/example/r0316137/mobieleapp3/HomeActivity.java
+++ app/src/main/java/com/example/r0316137/mobieleapp3/HomeActivity.java
@@ -2,12 +2,48 @@ package com.example.r0316137.mobieleapp3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+implements TextView.OnEditorActionListener,View.OnClickListener{
+
+
+    private EditText groupName;
+    private EditText className;
+    private Button startGame;
+    private Button scoreTabel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        //get references to the widgets
+        groupName = (EditText) findViewById(R.id.GroupName);
+        className = (EditText) findViewById(R.id.Klasgroup);
+        startGame = (Button) findViewById(R.id.Button02);
+        scoreTabel = (Button) findViewById(R.id.Button03);
+
+        // set the listeners
+        groupName.setOnEditorActionListener(this);
+        className.setOnEditorActionListener(this);
+        //startGame.
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        return false;
     }
 }
